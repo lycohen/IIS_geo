23974e9953f13a934d0513167dd7224e7025cc6a
Sebastian Wilwerth <sebastian.wilwerth@bancogalicia.com.ar>
2022-08-12 15:36:34 -0300

    Agrega vistas de explotación y consultas.

--- hql/consultas.sql
+++ hql/consultas.sql
@@ -18,11 +18,30 @@ FROM d_dl_tables.iis_geo_accesos acc
          INNER JOIN d_dl_tables.iis_geo_servicios ser on acc.servicio_id = ser.servicio_id
 GROUP BY acc.client_id, ser.canal, ser.host ORDER BY hits DESC;
 
--- hits por host
-SELECT count(hits) as hits, host FROM
+-- hits por canal / host
+SELECT count(hits) as hits, canal, host FROM
+(
+    SELECT count(*) as hits, acc.client_id, ser.canal, concat(ser.proto, '://', ser.host) as host
+    FROM d_dl_tables.iis_geo_accesos acc
+        INNER JOIN d_dl_tables.iis_geo_servicios ser on acc.servicio_id = ser.servicio_id
+    GROUP BY acc.client_id, ser.canal, host
+) ht GROUP BY host, canal ORDER BY hits DESC, canal, host;
+
+-- hits canal / host por hora
+SELECT count(hits) as hits, hora, canal, host FROM
     (
-        SELECT count(*) as hits, acc.client_id, ser.canal, concat(ser.proto, '://', ser.host) as host
+        SELECT count(*) as hits, acc.hora, acc.client_id, ser.canal, concat(ser.proto, '://', ser.host) as host
         FROM d_dl_tables.iis_geo_accesos acc
                  INNER JOIN d_dl_tables.iis_geo_servicios ser on acc.servicio_id = ser.servicio_id
-        GROUP BY acc.client_id, ser.canal, host ORDER BY hits DESC, acc.client_id
-    ) ht GROUP BY host;
+        GROUP BY acc.hora, acc.client_id, ser.canal, host
+    ) ht GROUP BY hora, canal, host ORDER BY hora DESC, hits DESC;
+
+
+-- mas de un país distinto de acceso para el mismo cliente
+SELECT DISTINCT * FROM d_dl_tables.iis_geo_accesos acc
+INNER JOIN d_dl_views.iis_geo_accesos_extranjero ext ON acc.client_id = ext.client_id
+WHERE acc.country = 'Argentina';
+
+-- ejemplo de acceso desde distintos países
+SELECT DISTINCT country, ts from d_dl_tables.iis_geo_accesos where client_id = 'cae12536f010f2eeb562205d800bee85c30cc2ea00e3002714193b415d735cdb';
+
--- hql/creates_impala.sql
+++ hql/creates_impala.sql
@@ -44,7 +44,7 @@ SELECT raw.ts
     INNER JOIN d_dl_tables.iis_geo_clientes cli
         ON cast(raw.document_number as bigint) = cast(cli.number as bigint) AND raw.document_type = cli.document_type
     INNER JOIN d_dl_tables.iis_geo_servicios ser
-        ON instr(raw.servicio, ser.canal) > 0 OR instr(raw.servicio, ser.host) > 0
+        ON instr(raw.servicio, ser.canal) > 0 OR instr(raw.servicio, concat(ser.host, '/')) > 0
     INNER JOIN d_dl_tables.iis_geo_segmentos seg
         ON instr(raw.segmento, seg.segmento) > 0;
 
@@ -54,3 +54,48 @@ CREATE TABLE IF NOT EXISTS d_dl_tables.iis_geo_accesos
     STORED AS PARQUET
     LOCATION 'hdfs://GALICIAHADOOP/galicia/d/landing_files/iis_geo/accesos'
 AS SELECT DISTINCT * FROM d_dl_views.iis_geo_accesos;
+
+-- DROP VIEW d_dl_views.iis_geo_accesos_pais
+CREATE VIEW IF NOT EXISTS d_dl_views.iis_geo_accesos_pais AS
+SELECT count(*) as hits, country FROM d_dl_tables.iis_geo_accesos GROUP BY country ORDER by hits DESC;
+
+-- DROP VIEW d_dl_views.iis_geo_accesos_segmentos
+CREATE VIEW IF NOT EXISTS d_dl_views.iis_geo_accesos_segmentos AS
+SELECT count(*) as hits, segmento FROM d_dl_tables.iis_geo_accesos GROUP BY segmento ORDER by hits DESC;
+
+-- DROP VIEW d_dl_views.iis_geo_accesos_canal
+CREATE VIEW IF NOT EXISTS d_dl_views.iis_geo_accesos_canal AS
+SELECT count(hits) as hits, canal, host FROM
+    (
+        SELECT count(*) as hits, acc.client_id, ser.canal, concat(ser.proto, '://', ser.host) as host
+        FROM d_dl_tables.iis_geo_accesos acc
+                 INNER JOIN d_dl_tables.iis_geo_servicios ser on acc.servicio_id = ser.servicio_id
+        GROUP BY acc.client_id, ser.canal, host
+    ) ht GROUP BY host, canal ORDER BY hits DESC, canal, host;
+
+-- DROP VIEW d_dl_views.iis_geo_accesos_hora
+CREATE VIEW IF NOT EXISTS d_dl_views.iis_geo_accesos_hora AS
+SELECT count(hits) as hits, hora, canal, host FROM
+    (
+        SELECT count(*) as hits, acc.hora, acc.client_id, ser.canal, concat(ser.proto, '://', ser.host) as host
+        FROM d_dl_tables.iis_geo_accesos acc
+                 INNER JOIN d_dl_tables.iis_geo_servicios ser on acc.servicio_id = ser.servicio_id
+        GROUP BY acc.hora, acc.client_id, ser.canal, host
+    ) ht GROUP BY hora, canal, host ORDER BY hora DESC, hits DESC;
+
+-- DROP VIEW d_dl_views.iis_geo_accesos_dayofweek
+CREATE VIEW IF NOT EXISTS d_dl_views.iis_geo_accesos_dayofweek AS
+SELECT count(hits) as hits, `dayofweek`, canal, host FROM
+    (
+        SELECT count(*) as hits, acc.`dayofweek`, acc.client_id, ser.canal, concat(ser.proto, '://', ser.host) as host
+        FROM d_dl_tables.iis_geo_accesos acc
+                 INNER JOIN d_dl_tables.iis_geo_servicios ser on acc.servicio_id = ser.servicio_id
+        GROUP BY acc.`dayofweek`, acc.client_id, ser.canal, host
+    ) ht GROUP BY `dayofweek`, canal, host ORDER BY `dayofweek`, hits DESC;
+
+-- DROP VIEW d_dl_views.iis_geo_accesos_extranjero
+CREATE VIEW IF NOT EXISTS d_dl_views.iis_geo_accesos_extranjero AS
+SELECT * FROM d_dl_tables.iis_geo_accesos where country != 'Argentina';
+
+
+
